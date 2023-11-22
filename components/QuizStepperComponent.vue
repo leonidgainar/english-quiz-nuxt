0ef779df7341f521c7817534cb05fa004dd5dee6
Leonid Gainar <leonid.smartdata.solutions@gmail.com>
2023-11-22 10:57:29 +0200

    Created dialog for requesting microphone access

--- components/QuizStepperComponent.vue
+++ components/QuizStepperComponent.vue
@@ -1,145 +1,150 @@
 <template>
-  <v-stepper v-model="step">
-    <v-stepper-header>
-      <v-stepper-step :complete="step > 1" step="1"> </v-stepper-step>
+  <div>
+    <v-stepper v-model="step">
+      <v-stepper-header>
+        <v-stepper-step :complete="step > 1" step="1"> </v-stepper-step>
 
-      <v-divider></v-divider>
+        <v-divider></v-divider>
 
-      <v-stepper-step :complete="step > 2" step="2"> </v-stepper-step>
+        <v-stepper-step :complete="step > 2" step="2"> </v-stepper-step>
 
-      <v-divider></v-divider>
+        <v-divider></v-divider>
 
-      <v-stepper-step step="3"> </v-stepper-step>
-    </v-stepper-header>
+        <v-stepper-step step="3"> </v-stepper-step>
+      </v-stepper-header>
 
-    <v-stepper-items>
-      <v-stepper-content step="1">
-        <div v-if="step === 1">
-          <h1>Welcome to {{ quizName }} Quiz!</h1>
-          <v-select
-            v-model.trim="numberOfQuestions"
-            :items="[5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 'All']"
-            label="Select number of questions"
-          ></v-select>
-          <v-row>
-            <v-col class="col-12 col-md-6 py-0">
-              <v-switch v-model="shuffleMode" label="Shuffle mode"></v-switch>
-            </v-col>
-            <v-col v-if="quizRequiresMicrophone" class="col-12 col-md-6 py-0">
-              <v-switch
-                v-model="isMicrophoneAllowed"
-                label="Allow microphone"
-              ></v-switch>
-            </v-col>
-          </v-row>
-          <v-btn
-            v-if="quizRequiresMicrophone"
-            class="mt-10"
-            color="primary"
-            :disabled="shouldDisableButton"
-            @click="startQuiz"
-          >
-            Start quiz
-          </v-btn>
-          <v-btn
-            v-else
-            class="mt-10"
-            color="primary"
-            :disabled="!numberOfQuestions"
-            @click="startQuiz"
-          >
-            Start quiz
-          </v-btn>
-          <v-alert class="mt-10" type="info">
-            Switch off shuffle mode if you want the verbs in the quiz to be in
-            alphabetical order
-          </v-alert>
-        </div>
-      </v-stepper-content>
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <div v-if="step === 1">
+            <h1>Welcome to {{ quizName }} Quiz!</h1>
+            <v-select
+              v-model.trim="numberOfQuestions"
+              :items="[5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 'All']"
+              label="Select number of questions"
+            ></v-select>
+            <v-row>
+              <v-col class="col-12 col-md-6 py-0">
+                <v-switch v-model="shuffleMode" label="Shuffle mode"></v-switch>
+              </v-col>
+              <v-col v-if="quizRequiresMicrophone" class="col-12 col-md-6 py-0">
+                <v-switch
+                  v-model="isMicrophoneAllowed"
+                  label="Allow microphone"
+                ></v-switch>
+              </v-col>
+            </v-row>
+            <v-btn
+              v-if="quizRequiresMicrophone"
+              class="mt-10"
+              color="primary"
+              :disabled="shouldDisableButton"
+              @click="startQuiz"
+            >
+              Start quiz
+            </v-btn>
+            <v-btn
+              v-else
+              class="mt-10"
+              color="primary"
+              :disabled="!numberOfQuestions"
+              @click="startQuiz"
+            >
+              Start quiz
+            </v-btn>
+            <v-alert class="mt-10" type="info">
+              Switch off shuffle mode if you want the verbs in the quiz to be in
+              alphabetical order
+            </v-alert>
+          </div>
+        </v-stepper-content>
 
-      <v-stepper-content step="2">
-        <div v-if="step === 2">
-          <StopwatchComponent :is-quiz-running="isQuizRunning" @timer-changed="onTimerChanged" />
-          <h2>
-            {{ currentIndex + 1 }}/{{ numberOfQuestions }}
-            {{ quizNames[quizId - 1] }}
-          </h2>
-          <h2 class="pt-5">
-            {{ quizQuestions[currentIndex].question }}
-          </h2>
-          <v-row v-if="quizRequiresMicrophone">
-            <v-col class="col-10">
+        <v-stepper-content step="2">
+          <div v-if="step === 2">
+            <StopwatchComponent :is-quiz-running="isQuizRunning" @timer-changed="onTimerChanged" />
+            <h2>
+              {{ currentIndex + 1 }}/{{ numberOfQuestions }}
+              {{ quizNames[quizId - 1] }}
+            </h2>
+            <h2 class="pt-5">
+              {{ quizQuestions[currentIndex].question }}
+            </h2>
+            <v-row v-if="quizRequiresMicrophone">
+              <v-col class="col-10">
+                <v-text-field
+                  v-model="currentAnswer"
+                  label="Your answer"
+                  placeholder="Speech your answer"
+                  readonly
+                ></v-text-field>
+              </v-col>
+              <v-col class="col-2 mt-5">
+                <v-icon v-if="!isRecognitionStarted" @click="startRecognition">
+                  mdi-microphone
+                </v-icon>
+                <v-icon v-else @click="endRecognition">
+                  mdi-stop-circle
+                </v-icon>
+              </v-col>
+            </v-row>
+            <v-form v-else @submit.prevent="nextQuestion">
               <v-text-field
                 v-model="currentAnswer"
                 label="Your answer"
-                placeholder="Speech your answer"
-                readonly
+                placeholder="Type your answer"
               ></v-text-field>
-            </v-col>
-            <v-col class="col-2 mt-5">
-              <v-icon v-if="!isRecognitionStarted" @click="startRecognition">
-                mdi-microphone
-              </v-icon>
-              <v-icon v-else @click="endRecognition">
-                mdi-stop-circle
-              </v-icon>
-            </v-col>
-          </v-row>
-          <v-form v-else @submit.prevent="nextQuestion">
-            <v-text-field
-              v-model="currentAnswer"
-              label="Your answer"
-              placeholder="Type your answer"
-            ></v-text-field>
-            <v-btn color="primary" :disabled="!currentAnswer" type="submit">
-              Next question
-            </v-btn>
-          </v-form>
-        </div>
-      </v-stepper-content>
+              <v-btn color="primary" :disabled="!currentAnswer" type="submit">
+                Next question
+              </v-btn>
+            </v-form>
+          </div>
+        </v-stepper-content>
 
-      <v-stepper-content step="3">
-        <div v-if="step === 3">
-          <h2>Your score: {{ score }}/{{ numberOfQuestions }}</h2>
-          <h3 class="text-right">
-            Quiz time:
-            {{ timer.hours }} : {{ timer.minutes }} : {{ timer.seconds }}
-          </h3>
-          <v-simple-table class="my-2">
-            <template #default>
-              <thead>
-                <tr>
-                  <th class="text-left">Verb</th>
-                  <th class="text-left">Correct answer</th>
-                  <th class="text-left">Your answer</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="verb in quizQuestions" :key="verb.question">
-                  <td>{{ verb.question }}</td>
-                  <td>{{ verb.correctAnswer }}</td>
-                  <td :class="verb.isCorrect ? 'green--text' : 'red--text'">
-                    {{ verb.yourAnswer }}
-                  </td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
-          <v-btn class="mt-2" color="primary" @click="startNewQuiz">
-            Start new quiz
-          </v-btn>
-        </div>
-      </v-stepper-content>
-    </v-stepper-items>
-  </v-stepper>
+        <v-stepper-content step="3">
+          <div v-if="step === 3">
+            <h2>Your score: {{ score }}/{{ numberOfQuestions }}</h2>
+            <h3 class="text-right">
+              Quiz time:
+              {{ timer.hours }} : {{ timer.minutes }} : {{ timer.seconds }}
+            </h3>
+            <v-simple-table class="my-2">
+              <template #default>
+                <thead>
+                  <tr>
+                    <th class="text-left">Verb</th>
+                    <th class="text-left">Correct answer</th>
+                    <th class="text-left">Your answer</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="verb in quizQuestions" :key="verb.question">
+                    <td>{{ verb.question }}</td>
+                    <td>{{ verb.correctAnswer }}</td>
+                    <td :class="verb.isCorrect ? 'green--text' : 'red--text'">
+                      {{ verb.yourAnswer }}
+                    </td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+            <v-btn class="mt-2" color="primary" @click="startNewQuiz">
+              Start new quiz
+            </v-btn>
+          </div>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+    <RequestMicrophoneAccessDialog :show-dialog="showMicrophoneAccessDialog" @close-dialog="showMicrophoneAccessDialog = false"/>
+  </div>
 </template>
 
 <script>
 import StopwatchComponent from './StopwatchComponent'
+import RequestMicrophoneAccessDialog from './RequestMicrophoneAccessDialog.vue'
 
 export default {
   components: {
     StopwatchComponent,
+    RequestMicrophoneAccessDialog,
   },
 
   props: {
@@ -168,6 +173,7 @@ export default {
         'Speech Past Simple form(s):',
         'Speech Past Participle form(s):',
       ],
+      showMicrophoneAccessDialog: false,
       recognition: null,
       isMicrophoneAllowed: false,
       isRecognitionStarted: false,
@@ -342,7 +348,7 @@ export default {
             this.isMicrophoneAllowed = true;
           } else if (permissionStatus.state === 'denied') {
             // Microphone access denied; instruct the user to enable it manually
-            alert('Microphone access is denied. Please enable it in your browser settings.');
+            this.showMicrophoneAccessDialog = true;
             this.isMicrophoneAllowed = false;
           } else {
             // Microphone access not determined; request permission
@@ -353,6 +359,7 @@ export default {
               })
               .catch(() => {
                 // Microphone access denied
+                this.showMicrophoneAccessDialog = true;
                 this.isMicrophoneAllowed = false;
               });
           }
---
+++ components/RequestMicrophoneAccessDialog.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="290">
+      <v-card>
+        <v-card-title class="text-h5">
+          Allow microphone
+        </v-card-title>
+        <v-card-text>Microphone access is denied. Please enable it in your browser settings.</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="closeDialog">
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    showDialog: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+  watch: {
+    showDialog() {
+      this.dialog = this.showDialog
+    },
+  },
+  methods: {
+    closeDialog() {
+      this.dialog = false
+      this.$emit('close-dialog')
+    },
+  }
+}
+</script>
